bba74083427744177652f932094b480e7d934438
Brandon McConnell <brandon@dreamthinkbuild.com>
2024-05-03 19:54:34 -0400

    Rebuild

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -4,15 +4,30 @@ type PossiblyInvoked<T extends (...args: any[]) => any> = T | ReturnType<T>;
 type TailwindPluginBase = typeof tailwindPlugin;
 type TailwindPlugin = ReturnType<TailwindPluginBase> | PossiblyInvoked<ReturnType<TailwindPluginBase['withOptions']>>;
 type TurbinePluginBase = {
-    transform?: (config: Partial<Config>) => Partial<Config>;
+    transform?: (config: NormalizedConfig) => NormalizedConfig;
     plugins?: TailwindPlugin[];
 };
 export type TurbinePlugin = TurbinePluginBase | ((...params: any[]) => TurbinePluginBase);
 type Plugin = TurbinePlugin | TailwindPlugin;
+type NonNullableTheme = NonNullable<Config['theme']>;
+interface NormalizedTheme extends NonNullableTheme {
+    supports: NonNullable<NonNullableTheme['supports']>;
+    data: NonNullable<NonNullableTheme['data']>;
+    colors: NonNullable<NonNullableTheme['colors']>;
+    spacing: NonNullable<NonNullableTheme['spacing']>;
+    container: NonNullable<NonNullableTheme['container']>;
+}
+interface NormalizedConfig extends Config {
+    safelist: NonNullable<Config['safelist']>;
+    blocklist: NonNullable<Config['blocklist']>;
+    presets: NonNullable<Config['presets']>;
+    theme: NormalizedTheme;
+    plugins: NonNullable<Config['plugins']>;
+}
 declare const Turbine: {
-    build({ config, plugins }: {
-        config: Partial<Config>;
+    build({ config: CONFIG_RAW, plugins, }: {
+        config: Config;
         plugins: Plugin[];
-    }): import("tailwindcss/types/config").Config;
+    }): NormalizedConfig;
 };
 export default Turbine;
